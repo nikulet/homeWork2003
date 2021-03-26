b6013bcd698e8a3c6797830c8acb561efaa9ed13
Nicu Ceban <nikulet01@gmail.com>
2021-03-26 14:35:53 +0200

    del

--- lessonsTekWill/EscapeSequence.java
+++
@@ -1,8 +0,0 @@
-package lessonsTekWill;
-
-public class EscapeSequence {
-    public static void main(String[] args) {
-        System.out.println("zd\\ffgv\tdfga\"cgada\buy\'juih\ngfyjghuhj\rdfcghfb\fseeetrygfhj");
-
-    }
-}
