5f8fd02d6df83d2b075f6e2de5541bc181192a95
Nicu Ceban <nikulet01@gmail.com>
2021-03-26 14:33:13 +0200

    some

---
+++ lessonsTekWill/EscapeSequence.java
@@ -0,0 +1,8 @@
+package lessonsTekWill;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("zd\\ffgv\tdfga\"cgada\buy\'juih\ngfyjghuhj\rdfcghfb\fseeetrygfhj");
+
+    }
+}
---
+++ lessonsTekWill/StandardOutputStreamWithString.java
@@ -0,0 +1,12 @@
+package lessonsTekWill;
+
+public class StandardOutputStreamWithString {
+    public static void main(String[] args) {
+        //creted a vareble of type String with a text
+        String myFavoritBook = new String("My favorite book is\"Fight Club\"by Chuck Palahniuk");
+        System.out.println(myFavoritBook);
+
+        String workFiles = new String("My work files are in D:\\Work Projects\\java");
+        System.out.println(workFiles);
+    }
+}
